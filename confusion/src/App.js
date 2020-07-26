62c215b1a076036ed04e75678de63d2c8c27377b
Farrukh <farrukhahmad62@gmail.com>
2020-07-26 14:41:23 +0500

    Components part 1

---
+++ confusion/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Farrukh</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+
+};
+
+export default App;
