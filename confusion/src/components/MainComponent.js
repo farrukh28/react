583d09b89f684696f497c9aea0e79b21d49bf27a
Farrukh <farrukhahmad62@gmail.com>
2020-08-22 18:14:55 +0500

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent'; // importing Header
 import Footer from './FooterComponent'; // importing Footer
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes'; // Importing shared Dishes file
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 
 class Main extends Component {
@@ -13,25 +15,27 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES, // Importing DISHES constant and assigning to dishes
-            selectedDish: null
         };
     };
 
-    onDishSelect(dishId) {
-        this.setState(
-            { selectedDish: dishId }
-        );
-    }
-
 
     render() {
+
+        // One way of defining components to use in Routers
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
         return (
             <div>
                 <Header />
-
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} /> {/* method to send dishes to Menu-component as props */}
-                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer />
             </div>
         );
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -9,7 +9,7 @@ function Menu(props) {  // Functional Component
     const menu = props.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => props.onClick(dish.id)}>
+                <Card>
                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
