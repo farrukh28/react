8e1823dbec516f4d9695e500cdcc2a79bbc0de6d
Farrukh <farrukhahmad62@gmail.com>
2020-08-23 18:53:26 +0500

    Single Page Applications Part 1

--- confusion/src/components/HomeComponent.js
+++ confusion/src/components/HomeComponent.js
@@ -1,4 +1,37 @@
 import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
+
+
+function RenderCard(props) {
+
+    var desig = () => {
+        if (props.item.designation != null) {
+            return (
+                <CardSubtitle className="font-weight-bold text-success">{props.item.designation}</CardSubtitle>
+            );
+        }
+        else {
+            return (null);
+        }
+    } /* Returns Desgination if Designation available otherwise return null */
+
+    return (
+        <>
+
+            <Card>
+                <CardImg src={props.item.image} alt={props.item.name} />
+                <CardBody>
+                    <CardTitle className="font-weight-bolder">{props.item.name}</CardTitle>
+                    {desig()}
+                    <CardText>{props.item.description}</CardText>
+
+                </CardBody>
+            </Card>
+
+        </>
+    );
+};
+
 
 
 function Home(props) {
@@ -6,7 +39,17 @@ function Home(props) {
     return (
         <>
             <div className="container">
-                <h4>Home</h4>
+                <div className="row align-items-start">
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={props.dish} />
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={props.promotion} />
+                    </div>
+                    <div className="col-12 col-md m-1">
+                        <RenderCard item={props.leader} />
+                    </div>
+                </div>
             </div>
         </>
     );
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,8 +3,12 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent'; // importing Header
 import Footer from './FooterComponent'; // importing Footer
+import Contact from './ContactComponent'; // importing contact
 import DishDetail from './DishDetailComponent';
 import { DISHES } from '../shared/dishes'; // Importing shared Dishes file
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 
@@ -15,6 +19,9 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES, // Importing DISHES constant and assigning to dishes
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS,
         };
     };
 
@@ -24,16 +31,20 @@ class Main extends Component {
         // One way of defining components to use in Routers
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         };
 
         return (
             <div>
                 <Header />
-                <Switch>
+                <Switch> {/*React Router*/}
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path="/contactus" component={() => <Contact />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
